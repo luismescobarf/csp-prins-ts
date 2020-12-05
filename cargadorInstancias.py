2136fad801b2420e8f55ade52c6831a65fb0ee3c
luismescobarf <luismescobarf@gmail.com>
2020-12-05 11:41:49 -0500

    Instancias e inicio lector

---
+++ cargadorInstancias.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  5 11:23:22 2020
+
+@author: luismiguelescobarfalcon
+"""
+
+#Función para cargar instancias csp
+def beasley(ruta):
+    pass
+
+
+#Llamados de diagnóstico
