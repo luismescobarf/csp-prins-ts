71cb27854a0b69a895639148815d2568d930c841
luismescobarf <luismescobarf@gmail.com>
2020-12-05 17:54:35 -0500

    Estructuración del contenedor

--- cargadorInstancias.py
+++ cargadorInstancias.py
@@ -1,109 +1,69 @@
 #Función para cargar instancias csp
 def beasley(ruta):
     
-    #Preparación de atributos de la instancia o caso de estudio
+    
+    #Preparación del contenedor tipo diccionario de la instancia
+    instancia = {}
+    instancia['bloques'] = [] #Listado de diccionarios de bloques de trabajo o servicios
     
     
     
-    #Ciclo general del recorrido del archivo mientras está abierto
-    with open(ruta) as manejadorArchivo:
-        coordenadasPuntos = []
-        for linea_n in manejadorArchivo:    
-            linea_n = linea_n.strip()
-            linea_n = linea_n.split(' ')     
-            coordenadasPuntos.append( [ int(linea_n[0]), float(linea_n[1]), float(linea_n[2]) ] )  
+    nombreCaso = str()
+    arcos = tuple()
     
-    numeroNodos = len(coordenadasPuntos)
     
-    #Construir matriz de adyacencia
-    matrizAdyacencia = []
-    for i in range(numeroNodos):
-        matrizAdyacencia.append( [0] *  numeroNodos)
+    
+    
+    
+    #Intentar abrir el archivo 
+    try:
         
-    for i in range(numeroNodos):
-        for j in range(numeroNodos):
-            if i!=j:
-                p_1 = [coordenadasPuntos[i][1], coordenadasPuntos[i][2]]
-                p_2 = [coordenadasPuntos[j][1], coordenadasPuntos[j][2]]
-                distanciaEuclidiana_p1_p2 = int((((p_1[0] - p_2[0])**2 + (p_1[1] - p_2[1])**2)**(1/2))+0.5)
-                matrizAdyacencia[i][j] = distanciaEuclidiana_p1_p2            
+        #Ciclo general del recorrido del archivo mientras está abierto
+        with open(ruta) as manejadorArchivo: 
+            
+            #Contador de líneas
+            contadorLineas = 0
+            
+         
+            coordenadasPuntos = []
+            for linea_n in manejadorArchivo: 
+                if not(contadorLineas):
+                    pass
+                
+                
+                
+                linea_n = linea_n.strip()
+                linea_n = linea_n.split(' ')     
+                coordenadasPuntos.append( [ int(linea_n[0]), float(linea_n[1]), float(linea_n[2]) ] )
+            
+    except:
+        return 'Error en la carga del archivo CSP!!'
+
+    
+    
+    
+    return instancia
+               
                 
     
-    #print(matrizAdyacencia) 
-    
-    #Distancia euclidiana (consultar)
-    #p_1 = [34,56]
-    #p_2 = [33,88]
-    #distanciaEuclidiana_p1_p2 = int(((p_1[0] - p_2[0])**2 + (p_1[1] - p_2[1])**2)**(1/2))
-    
-    #Parámetros de entrada (representación del problema)
-        
-    #Forzar grafo para los ejemplos
-    """
-    matrizAdyacencia=[]    
-    matrizAdyacencia = [	[0,5,8,4,9,12,5,3,13,22],
-    			[5,0,6,7,6,8,6,9,12,13],
-    			[8,6,0,9,10,12,14,11,9,21],
-    			[4,7,9,0,5,9,14,21,16,17],
-    			[9,6,10,5,0,8,12,10,9,16],
-    			[12,8,12,9,8,0,13,16,12,17],
-    			[5,6,14,14,12,13,0,9,11,13],
-    			[3,9,11,21,10,16,9,0,14,21],
-    			[13,12,9,16,9,12,11,14,0,8],
-    			[22,13,21,17,16,17,13,21,8,0]	]
-    numeroNodos = len(matrizAdyacencia)
-    """
+       
     
     
     
     
     
-    
-    
-    
-    pass
 
 
 #Llamados de diagnóstico
 
 
-"""
-//Establecer parámetros
-hb = 300;//Cantidad de minutos máximo por bloque
 
-//Este parámetro se obtiene desde las instancias de Beasley
-//ht = 600;//Cantidad de minutos máximo por turno
-
-extrab = 12;//Minutos extra permitidos al máximo de un bloque (5 horas)
-extrat = 20;//Minutos extra permitidos al máximo de un turno (10 horas)
-balance = 6;//Eventos o servicios mínimos en un bloque
-PosiblesBloques = 4;//Cantidad de bloques máxima en los que se puede partir una tabla (esclavo)						
-c = 1; //Costo asociado a la función objetivo de las columnas iniciales
-descanso = 30; //Descanso entre bloques
-descanson = 480; //Descanso entre dias
-
-////Conjunto de puntos de intercambio (parámetro establecido por código)
-//vector<string> puntosIntercambio;
-//puntosIntercambio.push_back("Viajero");
-//puntosIntercambio.push_back("Intercambiador_D/das");
-//puntosIntercambio.push_back("Intercambiador_Cuba");
-//puntosIntercambio.push_back("Estacion_Milan");					 
-//puntosIntercambio.push_back("Estacion_Cam");					 
-//puntosIntercambio.push_back("Integra_S.A.");					 
-//puntosIntercambio.push_back("Integra");					 
+				 
 
 /////////////////////
 // Lectura de datos//
 /////////////////////
 
-//Abrir el archivo con la matriz de costos e información de los depósitos
-ifstream data_input((testname).c_str());				
-if (! data_input.is_open() ) {
-	cout << "     Can not read the CSP IntegraUniAndes file " << (testname).c_str() << endl;
-	exit(1);
-	//return EXIT_FAILURE;
-	//return 1;
-}
 
 //Mostrar en pantalla la variante que se está trabajando
 cout<<endl<< "-- Repositorio Beasley 96 -- " <<endl;
@@ -256,4 +216,3 @@ cout<<endl;
 
 //Parar ejecución para visualzar salida en terminal	
 //getchar();
-"""
